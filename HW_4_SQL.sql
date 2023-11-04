b867c8a8a846cb17e927e1e2fe6cc3ff2a1d3ad9
schigolev.nikita <schigolev.nikita@wb.ru>
2023-11-04 19:54:51 -0400

    HW 4 SQL v2

--- HW_4_SQL.sql
+++ HW_4_SQL.sql
@@ -1,4 +1,5 @@
 -- Задание 1
+-- Заметим, что факт доставки подтверждается только в том случае, если заказ не отменен, поэтому считаем только заказы со статусом Approved
 SELECT 
 DISTINCT customer_id, name
 FROM orders
@@ -11,9 +12,11 @@ FROM orders
 order by diff DESC
 LIMIT 1)
 AND order_status = 'Approved'
-ORDER BY customer_id;
+ORDER BY customer_id
+LIMIT 1;
 
 -- Задание 2
+-- Также рассматриваем заказы только со статусом Approved
 SELECT 
 name,
 ROUND(AVG(EXTRACT(EPOCH FROM TO_TIMESTAMP(shipment_date, 'YYYY-MM-DD HH24:MI:SS') - TO_TIMESTAMP(order_date, 'YYYY-MM-DD HH24:MI:SS'))) / 86400) AS avg_days,
@@ -30,9 +33,11 @@ where order_status = 'Approved'
 group by customer_id
 Order by count(*) DESC
 LIMIT 1)
-ORDER BY sum(order_ammount) DESC
+ORDER BY sum(order_ammount) DESC;
 
--- Задание 3
+-- Задание 3 
+-- В данном задании создаем cte для двух выборок данных: количество подтвержденных заказов с задержкой и всех отмененных по пользователям
+
 WITH delay AS (SELECT 
 customer_id,
 count(*) as count_delay
@@ -53,7 +58,7 @@ COALESCE(count_delay, 0) + COALESCE(count_cancel, 0) as sum_orders
 from customers
 left OUTER join delay using (customer_id)
 left OUTER join cancel using (customer_id)
-order by sum_orders DESC
+order by sum_orders DESC;
 
 -- Задание 4
 WITH cat_amm AS (Select 
@@ -65,7 +70,7 @@ orders
 using(product_id)
 group by product_category),
 
-max_local_order AS (SELECT p.product_category, p.product_name AS max_order_ammount_product_id
+max_local_order AS (SELECT p.product_category, p.product_name AS max_order_ammount_product_id, P.product_id
 FROM products p
 JOIN (SELECT 
 p1.product_category, MAX(o1.order_ammount) AS max_order_ammount
@@ -81,7 +86,17 @@ select cat_amm.product_category, cat_amm.sum_category,
 FROM cat_amm 
 WHERE sum_category = (SELECT MAX(sum_category) FROM cat_amm)
 ) AS max_category,
-max_local_order.max_order_ammount_product_id
+max_local_order.max_order_ammount_product_id,
+product_id
 from cat_amm
-INNER join max_local_order on cat_amm.product_category = max_local_order.product_category
+INNER join max_local_order on cat_amm.product_category = max_local_order.product_category;
+
+-- Данный запрос можно проверить, найдя продукт с максимальной суммой продаж, например, для категории напитки
+Select * from 
+orders,
+products 
+where orders.product_id = products.product_id and 
+product_name in (select distinct product_name from products where product_category = 'Напитки')
+ORDER By order_ammount DESC
+LIMIT 1;
 
